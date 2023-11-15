37018f63e35de315225607cb27c8db001f7711f5
Haider Ali Anjum <haideralianjum@gmail.com>
2023-11-15 03:32:57 +0100

    fix: URL to repository

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
     <nav className='flex justify-between items-center h-16 bg-black px-6'>
       <h6 className='text-white text-s'>ZORA Front-end Assignment</h6>
 
-      <Link target='blank' href={"https://github.com/hdr-js"}>
+      <Link target='blank' href={"https://github.com/hdr-js/unsplash-search"}>
         <CodeBracketIcon
           className='h-5 w-5 text-gray-400 cursor-pointer'
           aria-hidden='true'
